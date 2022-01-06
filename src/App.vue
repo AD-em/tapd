e44e5a3c21244bc4e891d6b059a6f690a9e171db
Abel Endeshaw <contact.abelend@gmail.com>
2022-01-06 11:30:25 +0300

    Feat: added vuetify and some boilerplate code with minimal clock functionality

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,36 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" width="25%">
-    <HelloWorld msg="Hello Vue in CodeSandbox!"/>
-  </div>
+  <v-app>
+    <v-app-bar app color="indigo lighten-1" dark>
+      <div class="d-flex align-center text-h4">
+        <span class="amber--text text--lighten-2">+</span>Apd
+      </div>
+
+      <v-spacer></v-spacer>
+
+      <v-btn outlined class="amber lighten-1 indigo--text">
+        <span class="mr-2">Sign in</span> </v-btn
+      ><v-btn text class="amber--text text--lighten-1">
+        <span class="mr-2">Sign up</span>
+      </v-btn>
+    </v-app-bar>
+
+    <v-main class="mt-3">
+      <Timer />
+      <TaskList />
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld";
-
+import Timer from "@/components/Timer";
+import TaskList from "@/components/TaskList";
 export default {
   name: "App",
-  components: {
-    HelloWorld
-  }
+
+  components: { Timer, TaskList },
+
+  data: () => ({
+    //
+  }),
 };
 </script>
-
-<style>
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/TaskCard.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-card>
+      task-card
+  </v-card>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
