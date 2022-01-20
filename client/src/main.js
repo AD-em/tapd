dd4286e0aed012db3a888d803520be4375ac5e0c
AD-em <adem164416@gmail.com>
2022-01-20 09:50:20 +0300

    Added pm2 to the workflow file

--- client/src/main.js
+++ client/src/main.js
@@ -1,15 +1,17 @@
-import Vue from "vue";
-import App from "./App.vue";
-import vuetify from './plugins/vuetify'
-import '@babel/polyfill'
-import router from './router'
-import store from './store'
+import Vue from 'vue';
+import App from './App.vue';
+import vuetify from './plugins/vuetify';
+import '@babel/polyfill';
+import router from './router';
+import store from './store';
 
 Vue.config.productionTip = false;
+
+console.log(`And our GitHub Actions CI/CD setup is complete babes.`);
 
 new Vue({
   vuetify,
   router,
   store,
-  render: h => h(App)
-}).$mount("#app");
+  render: (h) => h(App)
+}).$mount('#app');
