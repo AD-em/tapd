f016a12d788c700336c75582f3751e27413131aa
Abigya Getachew <Abiguget18@gmail.com>
2022-02-01 14:14:16 +0300

    Feature: Added re-route after signin

--- client/src/components/Signin.vue
+++ client/src/components/Signin.vue
@@ -2,7 +2,9 @@
   <v-form class="ml-n16">
     <v-text-field v-model="email" label="Email" required></v-text-field>
     <v-text-field v-model="password" label="Password" required></v-text-field>
-    <v-btn class="mr-4 cyan" @click="signin"> Sign in </v-btn>
+    <v-btn class="mr-4 indigo lighten-1 white--text" @click="signin">
+      Sign in
+    </v-btn>
   </v-form>
 </template>
 
@@ -17,8 +19,11 @@ export default {
     signin() {
       TaskService.signin(this.email, this.password)
         .then((res) => {
-          if (res.data.data) this.$store.state.user = res.data.data.signin;
-          else console.debug(res.data.errors);
+          if (res.data.data) {
+            this.$store.state.user = res.data.data.signin;
+            sessionStorage.setItem("user", this.$store.state.user);
+            this.$router.push({ name: "task-list" });
+          } else console.debug(res.data.errors);
           console.dir(this.$store.state.user.email);
         })
         .catch((err) => console.error(err));
